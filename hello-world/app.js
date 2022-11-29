286dbb3deedee357389023a613b1e1a924ee0f45
Taran Rickard <taranrickard@gmail.com>
2022-11-28 20:40:01 -0500

    getting started

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,8 +1,8 @@
-import { Statement } from './dynamo.js'
+import { Statement } from '/opt/nodejs/dynamodb'
 
 export async function lambdaHandler(event) {
 
-    const data = await Statement('SELECT * FROM products')
+    const data = await Statement('SELECT id, name FROM products')
     const response = {
         statusCode: 200,
         body: JSON.stringify(data)
